0f9c99a8fab496a2a79ff102066f9b7a10a09633
DI-AQUARIUS <aquarius@aquarius-air.local>
2022-04-23 12:45:12 +0900

    authSlice.tsの編集: Django-APIにアクセスするためのReduxThunk非同期関数の作成

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -10,15 +10,97 @@ import { PROPS_AUTHEN, PROPS_PROFILE, PROPS_NICKNAME } from "../types";
 const apiUrl = process.env.REACT_APP_DEV_API_URL;
 
 /*
-export const incrementAsync = createAsyncThunk(
-  "counter/fetchCount",
-  async (amount: number) => {
-    const response = await fetchCount(amount);
-    // The value we return becomes the `fulfilled` action payload
-    return response.data;
+以下のfetchAsyncLoginがコンポーネント側から呼び出された際、{email, password}を引数として渡す。
+これが引数authen に入ってくる。PROPS_AUTHEN型 は、別途types.ts ファイルに定義されている。
+ */
+export const fetchAsyncLogin = createAsyncThunk(
+  "auth/post",
+  async (authen: PROPS_AUTHEN) => {
+    const res = await axios.post(`${apiUrl}authen/jwt/create`, authen, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
   }
 );
+
+export const fetchAsyncRegister = createAsyncThunk(
+  "auth/register",
+  async (auth: PROPS_AUTHEN) => {
+    const res = await axios.post(`${apiUrl}api/register/`, auth, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
+  }
+);
+
+/*
+このアプリの仕様では、最初に作るProfile情報には、nickNameだけを登録して、画像データはnullで作られる。
+また、Django側でProfileのアクセスを受けるViewsは、JWT認証していないとアクセスできないので、
+headersに JWT Token を含めてDjango側のProfileViewsにアクセスする。
  */
+export const fetchAsyncCreateProf = createAsyncThunk(
+  "profile/post",
+  async (nickName: PROPS_NICKNAME) => {
+    const res = await axios.post(`${apiUrl}api/profile/`, nickName, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+/* Profileの更新 */
+export const fetchAsyncUpdateProf = createAsyncThunk(
+  "profile/put",
+  async (profile: PROPS_PROFILE) => {
+    const uploadData = new FormData();
+
+    uploadData.append("nickName", profile.nickName);
+    profile.img && uploadData.append("img", profile.img, profile.img.name);
+    const res = await axios.put(
+      `${apiUrl}api/profile/${profile.id}/`,
+      uploadData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      }
+    );
+    return res.data;
+  }
+);
+
+/* LoginUser自身のProfileを取得する非同期関数
+最後の rturn res.data[0] は、
+Django側のviews.pyのMyProfileListViewでの戻り値return が、filterで絞り込むので配列型の値が
+responseとして帰ってくるため、配列の最初０番を指定することで値を取り出している。
+*/
+export const fetchAsyncGetMyProf = createAsyncThunk("profile/get", async () => {
+  const res = await axios.get(`${apiUrl}api/myprofile/`, {
+    headers: {
+      Authorization: `JWT ${localStorage.localJWT}`,
+    },
+  });
+  return res.data[0];
+});
+
+/* 存在する全てのuserのProfileを取得する */
+export const fetchAsyncGetProfs = createAsyncThunk("profiles/get", async () => {
+  const res = await axios.get(`${apiUrl}api/profile/`, {
+    headers: {
+      Authorization: `JWT ${localStorage.localJWT}`,
+    },
+  });
+  return res.data;
+});
+
 export const authSlice = createSlice({
   name: "auth",
   initialState: {
